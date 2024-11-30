2f1662964355464d40438e718a11f831ee6207a3
anup <116241437+anupk5743@users.noreply.github.com>
2024-11-30 16:12:58 +0000

    data types

---
+++ dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all JS code as never version
+//alert(3 + 3) - we are using nodejs,  not browser
+
+console.log(3 + 3) ; //code readability should be high
+console.log("anup")
