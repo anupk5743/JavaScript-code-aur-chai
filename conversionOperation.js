6774b8e895df6da9be180a53fb2eb224500b21ca
anup <116241437+anupk5743@users.noreply.github.com>
2024-11-30 16:13:40 +0000

    conversion datatypes

---
+++ conversionOperation.js
@@ -0,0 +1,16 @@
+let score = '33aaa'
+console.log(typeof score);
+console.log(typeof(score))
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+ // "33" => 33
+ // "33aaa" => NaN
+
+ let isLoggedIn = 1
+ let booleansIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleansIsLoggedIn)
+
+ // 1 => true; 0 => false
+ // "" => false
+ // "hitesh" => true
